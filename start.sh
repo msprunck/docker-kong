8f4d69b1eb759dc7f507d37bfe6ef2800ce9b7e8
Matthieu Sprunck <matthieu@sprunck.com>
2015-11-17 21:42:29 +0100

    Fix cassandra host replacement

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # Set cassandra host and port
 if [ ! -z "$CASSANDRA_HOST" ]; then
     echo "cassandra host: $CASSANDRA_HOST"
-    sed -i -r 's/cassandra:9042/$CASSANDRA_HOST/g' /etc/kong/kong.yml
+    sed -i -r "s/cassandra:9042/$CASSANDRA_HOST/g" /etc/kong/kong.yml
 fi
 
 kong start
